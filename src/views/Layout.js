3df239b69a8a0b2d968c115f957dcffec6149b1f
Ademar Arvati <arvati@hotmail.com>
2019-04-15 20:35:49 -0300

    codesandbox
    working at codesandbox

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,5 +1,5 @@
 // src/views/Layout.js
-var m = require("mithril").default
+var m = require("mithril")
 //const m = require('mithril').default
 
 module.exports = {
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,5 +1,5 @@
 // src/views/UserList.js
-var m = require("mithril").default
+var m = require("mithril")
 //const m = require('mithril').default
 
 var User = require("../models/User")
